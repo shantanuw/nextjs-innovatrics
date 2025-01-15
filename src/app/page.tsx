dd19e81b970c59ce6ba970454b5c285244f0887d
Shantanu Wagh <shantanuw@gmail.com>
2025-01-15 13:09:14 -0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,131 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import type {
+  CallbackImage,
+  DocumentCallback,
+  DocumentComponentData,
+} from "@innovatrics/dot-document-auto-capture";
+import type {
+  FaceCallback,
+  FaceComponentData,
+} from "@innovatrics/dot-face-auto-capture";
+import type { MagnifEyeLivenessCallback } from "@innovatrics/dot-magnifeye-liveness";
+import { SmileLivenessCallback } from "@innovatrics/dot-smile-liveness";
+import { useCallback, useState } from "react";
+import ComponentSelect from "./components/ComponentSelect";
+import DocumentAutoCapture from "./components/DocumentAutoCapture";
+import FaceAutoCapture from "./components/FaceAutoCapture";
+import MagnifEyeLiveness from "./components/MagnifEyeLiveness";
+import PhotoResult from "./components/PhotoResult";
+import SmileLiveness from "./components/SmileLiveness";
+import styles from "./styles/index.module.css";
+import { Step } from "./types";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+function App() {
+  const [step, setStep] = useState<Step>(Step.SELECT_COMPONENT);
+  const [photoUrl, setPhotoUrl] = useState<string>();
+
+  const handlePhotoTaken = <T,>(
+    imageData: CallbackImage<T>,
+    content?: Uint8Array,
+  ) => {
+    const imageUrl = URL.createObjectURL(imageData.image);
+    setPhotoUrl(imageUrl);
+  };
+
+  const handleDocumentPhotoTaken: DocumentCallback = (imageData, content) => {
+    handlePhotoTaken(imageData, content);
+  };
+
+  const handleFaceCapturePhotoTaken: FaceCallback = (imageData, content) => {
+    handlePhotoTaken(imageData, content);
+  };
+
+  /**
+   * At this point use @content property with Digital Identity Service in order to evaluate the MagnifEye liveness score.
+   * See: https://developers.innovatrics.com/digital-onboarding/technical/remote/dot-dis/latest/documentation/#_magnifeye_liveness_check
+   */
+  const handleMagnifEyeComplete: MagnifEyeLivenessCallback = (
+    imageData,
+    content,
+  ) => {
+    handlePhotoTaken(imageData, content);
+  };
+
+  /**
+   * At this point use @content property with Digital Identity Service in order to evaluate the Smile liveness score.
+   */
+  const handleSmileComplete: SmileLivenessCallback = (imageData, content) => {
+    const [, smileImageData] = imageData;
+    handlePhotoTaken(smileImageData, content);
+  };
+
+  const handleError = useCallback((error: Error) => {
+    alert(error);
+  }, []);
+
+  const handleBackClick = () => {
+    setPhotoUrl(undefined);
+    setStep(Step.SELECT_COMPONENT);
+  };
+
+  const renderStep = (currentStep: Step) => {
+    switch (currentStep) {
+      case Step.DOCUMENT_CAPTURE:
+        return (
+          <>
+            <DocumentAutoCapture
+              onPhotoTaken={handleDocumentPhotoTaken}
+              onError={handleError}
+              onBackClick={handleBackClick}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            {photoUrl && <PhotoResult photoUrl={photoUrl} />}
+          </>
+        );
+      case Step.FACE_CAPTURE:
+        return (
+          <>
+            <FaceAutoCapture
+              onPhotoTaken={handleFaceCapturePhotoTaken}
+              onError={handleError}
+              onBackClick={handleBackClick}
+            />
+            {photoUrl && <PhotoResult photoUrl={photoUrl} />}
+          </>
+        );
+      case Step.MAGNIFEYE_LIVENESS:
+        return (
+          <>
+            <MagnifEyeLiveness
+              onComplete={handleMagnifEyeComplete}
+              onError={handleError}
+              onBackClick={handleBackClick}
+            />
+            {photoUrl && <PhotoResult photoUrl={photoUrl} />}
+          </>
+        );
+      case Step.SMILE_LIVENESS:
+        return (
+          <>
+            <SmileLiveness
+              onComplete={handleSmileComplete}
+              onError={handleError}
+              onBackClick={handleBackClick}
+            />
+            {photoUrl && <PhotoResult photoUrl={photoUrl} />}
+          </>
+        );
+      default:
+        return <ComponentSelect setStep={setStep} />;
+    }
+  };
+
+  return (
+    <div className={styles.app}>
+      <h1>DOT Web Components Integration</h1>
+      {renderStep(step)}
     </div>
   );
 }
+
+export default App;
